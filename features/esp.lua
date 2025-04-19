14063c479670245ac796d0cefce38ddc62ddef49
semi556tgb <dredayvon@Gmail.com>
2025-04-19 11:39:57 -0500

    Update esp.lua

--- features/esp.lua
+++ features/esp.lua
@@ -1,1 +1,24 @@
 -- esp.lua content
+local ESP = {}
+
+function ESP:Init(tab)
+    local Section = tab:AddSection("ESP Settings")
+
+    Section:AddToggle({
+        text = "Enable ESP",
+        flag = "esp_enabled",
+        state = false
+    })
+
+    Section:AddSlider({
+        text = "ESP Distance",
+        min = 50,
+        max = 1000,
+        float = 0,
+        flag = "esp_distance",
+        suffix = " studs",
+        state = 500
+    })
+end
+
+return ESP
