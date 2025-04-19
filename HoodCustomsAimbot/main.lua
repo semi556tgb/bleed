172a48ba2f624708718af2b71eb78284615d7830
semi556tgb <dredayvon@Gmail.com>
2025-04-19 10:48:30 -0500

    Delete HoodCustomsAimbot directory

--- HoodCustomsAimbot/main.lua
+++
@@ -1,5 +0,0 @@
-
--- Your full script content (from the canvas) would be placed here.
--- For this example, I will assume it was saved into a variable for now.
-
--- Save your entire Roblox aimbot code in 'main.lua' inside the project folder
