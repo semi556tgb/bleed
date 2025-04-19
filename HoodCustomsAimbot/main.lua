0c02e268de57090852afdd89292109eba60c0c2d
semi556tgb <dredayvon@Gmail.com>
2025-04-19 10:45:58 -0500

    Add files via upload

---
+++ HoodCustomsAimbot/main.lua
@@ -0,0 +1,5 @@
+
+-- Your full script content (from the canvas) would be placed here.
+-- For this example, I will assume it was saved into a variable for now.
+
+-- Save your entire Roblox aimbot code in 'main.lua' inside the project folder
